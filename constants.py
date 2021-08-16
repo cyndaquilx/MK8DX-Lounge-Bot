58a9e3cc31ae2abcee453fec1323685bdf2f33e8
cyndaquilx <46006425+cyndaquilx@users.noreply.github.com>
2021-08-16 00:14:52 -0400

    Update constants.py

--- constants.py
+++ constants.py
@@ -5,11 +5,15 @@ bot_channels = [741906846209671223]
 channels = {"X": 698153967820996639,
             "S": 445716741830737920,
             "A": 445570804915109889,
-            "AB": 817605040105717830,
+            #"AB": 817605040105717830,
             "B": 445570790151421972,
+            "BC": 874395278520774678,
             "C": 445570768269475840,
+            "CD": 874395385525854318,
             "D": 445570755657465856,
+            "DE": 874395482045153322,
             "E": 445716908923420682,
+            "EF": 874395541482647592,
             "F": 796870494405394472,
             "SQ": 772531512410636289}
 
@@ -18,81 +22,115 @@ channels = {"X": 698153967820996639,
 ranks = {
     "Grandmaster": {
         "emoji": "<:grandmaster:731579876846338161>",
-        "roleid": 730976842898735195,
+        "roleid": 874340227177668699,
         "color": "#A3022C",
         "url": "https://i.imgur.com/EWXzu2U.png"},
     "Master": {
         "emoji": "<:master:731597294914502737>",
-        "roleid": 445707276385386497,
+        "roleid": 874340298048831578,
         "color": "#D9E1F2",
         "url": "https://i.imgur.com/3yBab63.png"},
-    "Diamond": {
-        "emoji": "<:diamond:731579813386780722>",
-        "roleid": 445404401989844994,
+    "Diamond 2": {
+        "emoji": "<:diamond:731579813386780722> 2",
+        "roleid": 874340374083154030,
+        "color": "#BDD7EE",
+        "url": "https://i.imgur.com/RDlvdvA.png"},
+    "Diamond 1": {
+        "emoji": "<:diamond:731579813386780722> 1",
+        "roleid": 874340476080238612,
         "color": "#BDD7EE",
         "url": "https://i.imgur.com/RDlvdvA.png"},
     "Sapphire": {
         "emoji": "<:sapphire:731579851802411068>",
-        "roleid": 730976660681130075,
+        "roleid": 874340543923118130,
         "color": "#286CD3",
         "url": "https://i.imgur.com/bXEfUSV.png"},
-    "Platinum": {
-        "emoji": "<:platinum:542204444302114826>",
-        "roleid": 445544728700649472,
+    "Platinum 2": {
+        "emoji": "<:platinum:542204444302114826> 2",
+        "roleid": 874340619751931934,
         "color": "#3FABB8",
         "url": "https://i.imgur.com/8v8IjHE.png"},
-    "Gold": {
-        "emoji": "<:gold:731579798111125594>",
-        "roleid": 445404441110380545,
+    "Platinum 1": {
+        "emoji": "<:platinum:542204444302114826> 1",
+        "roleid": 874340697887637535,
+        "color": "#3FABB8",
+        "url": "https://i.imgur.com/8v8IjHE.png"},
+    "Gold 2": {
+        "emoji": "<:gold:731579798111125594> 2",
+        "roleid": 874340761964003389,
         "color": "#FFD966",
         "url": "https://i.imgur.com/6yAatOq.png"},
-    "Silver": {
-        "emoji": "<:silver:731579781828575243>",
-        "roleid": 445544735638159370,
+    "Gold 1": {
+        "emoji": "<:gold:731579798111125594> 1",
+        "roleid": 874340824861794324,
+        "color": "#FFD966",
+        "url": "https://i.imgur.com/6yAatOq.png"},
+    "Silver 2": {
+        "emoji": "<:silver:731579781828575243> 2",
+        "roleid": 874340970764861511,
         "color": "#D9D9D9",
         "url": "https://i.imgur.com/xgFyiYa.png"},
-    "Bronze": {
-        "emoji": "<:bronze:731579759712010320>",
-        "roleid": 445404463092596736,
+    "Silver 1": {
+        "emoji": "<:silver:731579781828575243> 1",
+        "roleid": 874341090579349504,
+        "color": "#D9D9D9",
+        "url": "https://i.imgur.com/xgFyiYa.png"},
+    "Bronze 2": {
+        "emoji": "<:bronze:731579759712010320> 2",
+        "roleid": 874341171399376896,
+        "color": "#C65911",
+        "url": "https://i.imgur.com/DxFLvtO.png"},
+    "Bronze 1": {
+        "emoji": "<:bronze:731579759712010320> 1",
+        "roleid": 874342922601005066,
         "color": "#C65911",
         "url": "https://i.imgur.com/DxFLvtO.png"},
     "Iron 2": {
         "emoji": "<:iron:731579735544430703> 2",
-        "roleid": 730976738007580672,
+        "roleid": 874343078784274502,
         "color": "#817876",
         "url": "https://i.imgur.com/AYRMVEu.png"},
     "Iron 1": {
         "emoji": "<:iron:731579735544430703> 1",
-        "roleid": 805288798879481886,
+        "roleid": 874343146316783708,
         "color": "#817876",
         "url": "https://i.imgur.com/AYRMVEu.png"}
     }
 
 place_MMRs = {"gold": 7500,
-              "silver": 6000,
-              "bronze": 4500,
-              "iron2": 3000,
-              "iron1": 1750}
+              "silver": 5500,
+              "bronze": 3500,
+              "iron": 1500}
 
 #this is where you define the MMR thresholds for each rank
 def getRank(mmr: int):
-    if mmr >= 14500:
+    if mmr >= 14000:
         return("Grandmaster")
     elif mmr >= 13000:
         return("Master")
-    elif mmr >= 11500:
-        return("Diamond")
+    elif mmr >= 12000:
+        return("Diamond 2")
+    elif mmr >= 11000:
+        return("Diamond 1")
     elif mmr >= 10000:
         return("Sapphire")
-    elif mmr >= 8500:
-        return("Platinum")
+    elif mmr >= 9000:
+        return("Platinum 2")
+    elif mmr >= 8000:
+        return("Platinum 1")
     elif mmr >= 7000:
-        return("Gold")
-    elif mmr >= 5500:
-        return("Silver")
+        return("Gold 2")
+    elif mmr >= 6000:
+        return("Gold 1")
+    elif mmr >= 5000:
+        return("Silver 2")
     elif mmr >= 4000:
-        return("Bronze")
+        return("Silver 1")
+    elif mmr >= 3000:
+        return("Bronze 2")
     elif mmr >= 2000:
+        return("Bronze 1")
+    elif mmr >= 1000:
         return("Iron 2")
     else:
         return ("Iron 1")
